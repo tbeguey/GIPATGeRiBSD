febb62f982f3f85ad2664a0f70d34294874fdbaa
tbeguey <begueytheo@gmail.com>
2017-05-22 17:59:46 +0200

    requete de selection

---
+++ selectInCommons.sql
@@ -0,0 +1,6 @@
+﻿select geoserver_xml.title, metadata.title, couche_libelle, type_donnees_echange_libelle
+from communs.correspondance
+join geoserver.geoserver_xml on idgeoserver = idcouche
+join geonetwork.metadata on idgeonetwork = uuid
+join cartogip.couche on idcartogip = id_couche
+join bsd.type_donnees_echange on idbsd = no_type_donnees_echange;
